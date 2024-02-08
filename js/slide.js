18d85968735bb54a720180955e2e0d4c51288d4a
pcm9881 <pcm9881@gmail.com>
2024-02-08 09:00:52 +0000

    deploy: pcm9881/pcm9881.github.io.generator@0a1180bebd27b4f1edfd515f0420e1f1b1015a23

--- js/slide.js
+++ js/slide.js
@@ -1,29 +1,38 @@
-const slide = document.querySelector("#slide");
-const prevBtn = document.querySelector(".arrow-prev");
-const nextBtn = document.querySelector(".arrow-next");
-let currentPosition = 0;
-const step = 600;
+const slide = document.getElementById("slide");
+const slideItems = document.querySelectorAll(".slide-item");
+const prev = document.querySelector(".arrow-prev");
+const next = document.querySelector(".arrow-next");
+const step = 400;
+let current = 0;
+let max = 4;
 
-function moveSlider(direction) {
-  const currentLeft = slide.style.left;
+(function () {
+  prev.addEventListener("click", function () {
+    shiftSlide("prev");
+  });
 
-  if ((currentLeft === "" || currentLeft === "0px") && direction === "prev") {
-    currentPosition = -2400;
-  } else if (direction === "prev") {
-    currentPosition += step;
-  } else {
-    currentPosition -= step;
+  next.addEventListener("click", function () {
+    shiftSlide("next");
+  });
+
+  function shiftSlide(direction) {
+    if (direction === "prev") {
+      if (current === 0) {
+        current = max;
+      } else {
+        current--;
+      }
+    } else {
+      if (current === max) {
+        current = 0;
+      } else {
+        current++;
+      }
+    }
+
+    slideItems.forEach((slideItem, index) => {
+      // console.log(index, current, index - current);
+      slideItem.style.transform = `translateX(${-400 * current}px)`;
+    });
   }
-
-  slide.style.left = currentPosition + "px";
-}
-
-console.log(prevBtn);
-
-prevBtn.addEventListener("click", function () {
-  moveSlider("prev");
-});
-
-nextBtn.addEventListener("click", function () {
-  moveSlider("next");
-});
+})();
